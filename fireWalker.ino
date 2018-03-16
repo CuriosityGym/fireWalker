ddf2dcec3df3b18fe392c15267e322c1f4f3b1cd
siddheshmurudkar <32839650+siddheshmurudkar@users.noreply.github.com>
2018-03-16 12:28:11 +0530

    Update fireWalker.ino

--- fireWalker.ino
+++ fireWalker.ino
@@ -1,11 +1,4 @@
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-  This example code is in the public domain.
-*/
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
